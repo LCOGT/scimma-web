221cc665963b8b2e61c5e71bff3cd9b205d0928a
David Collom <dcollom@lco.global>
2020-04-01 12:17:50 -0700

    Adding endpoints and docstrings

--- app.py
+++ app.py
@@ -18,17 +18,38 @@ KAFKA_PORT = '9092'
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
+    """
+    Returns the list of topics and number of messages per topic
+    """
     # with stream.open('kafka://localhost:9092/gcn')
     return 'index'
 
 
 @app.route('/publish', methods=['GET', 'POST'])
 def publish():
+    """
+    Allows a user to publish an alert to the given topic
+    """
     form = PublishForm(request.form)
     if request.method == 'POST' and form.validate():
         with Stream.open('kafka://{KAFKA_HOST}:{KAFKA_PORT}/{form.topic.data}', 'w', format='json') as s:
             s.write({'content': form.content.data})
     return render_template('publish_form.html', form=form)
-    
+
+
+@app.route('/topic/<str>', methods=['GET'])
+def topic(pk):
+    """
+    Returns the messages for a specific topic
+    """
+    return 'topic'
+
+
+@app.route('/topic/<str>/message/<id>', methods=['GET'])
+def message(pk):
+    """
+    Displays the content of a specific message
+    """
+    return 'message'
