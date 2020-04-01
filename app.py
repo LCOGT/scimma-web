b6960b7501b893538ce01c63780d9a175ca9ecc5
David Collom <dcollom@lco.global>
2020-04-01 15:35:42 -0700

    added topic list to scimma client wrapper

--- app.py
+++ app.py
@@ -2,15 +2,21 @@ from flask import Flask, render_template, request
 
 from scimma.client.io import Stream
 
+from client import ScimmaClientWrapper
 from forms import PublishForm
-
 
 # KAFKA_HOST = 'localhost'
 KAFKA_HOST = 'firkraag.lco.gtn'
 KAFKA_PORT = '9092'
 
 
+kafka_config = {
+    'bootstrap.servers': 'firkraag.lco.gtn:9092',
+    'group.id': 'scimma-web-test'
+}
+
 app = Flask(__name__)
+client_wrapper = ScimmaClientWrapper(**kafka_config)
 
 
 @app.route('/', methods=['GET'])
@@ -28,6 +34,7 @@ def publish():
     Allows a user to publish an alert to the given topic
     """
     form = PublishForm(request.form)
+    form.topic.choices = [(topic, topic) for topic in client_wrapper.topics()]
     if request.method == 'POST' and form.validate():
         with Stream.open('kafka://{KAFKA_HOST}:{KAFKA_PORT}/{form.topic.data}', 'w', format='json') as s:
             s.write({'content': form.content.data})
---
+++ client.py
@@ -0,0 +1,13 @@
+from confluent_kafka import Consumer, Producer
+from confluent_kafka.admin import AdminClient
+
+
+class ScimmaClientWrapper:
+    def __init__(self, *args, **opts):
+        print(opts)
+        self.consumer = Consumer(opts)
+        self.producer = Producer(opts)
+        self.admin_client = AdminClient(opts)
+
+    def topics(self):
+        return [topic_name for topic_name, topic_metadata in self.admin_client.list_topics().topics.items()]
