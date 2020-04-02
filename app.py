b7932bf4d48c9534fab963b3154df1298460b161
David Collom <dcollom@lco.global>
2020-04-02 16:21:31 -0700

    moved everything and avoided import hell

--- app.py
+++ app.py
@@ -1,71 +1,29 @@
-from datetime import datetime
+from flask import Flask
 
-from flask import Flask, render_template, request
-
-from scimma.client import stream
-
-from client import ScimmaClientWrapper
-from forms import PublishForm
-
-KAFKA_HOST = 'localhost'
-# KAFKA_HOST = 'firkraag.lco.gtn'
-KAFKA_PORT = '9092'
+from config import config
 
 
-kafka_config = {
-    'bootstrap.servers': f'{KAFKA_HOST}:{KAFKA_PORT}',
-    'group.id': 'scimma-web-test'
-}
-
-app = Flask(__name__)
-client_wrapper = ScimmaClientWrapper(**kafka_config)
-
-
-@app.route('/topic', methods=['GET'])
-def topic_list():
-    """
-    Returns the list of topics and number of messages per topic
-    """
-    topics = client_wrapper.topics()
-    return f'topics: {topics}'
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(config)
+    register_extensions(app)
+    register_blueprints(app)
+    return app
 
 
-@app.route('/topic/<id>', methods=['GET'])
-def topic_get(id):
-    """
-    Returns the messages for a specific topic
-    """
-    messages = []
+def register_extensions(app):
+    from extensions import db
 
-    # 'kafka://firkraag.lco.gtn:9092/test',
-    with stream.open(f'kafka://{KAFKA_HOST}:{KAFKA_PORT}/{id}', 'r',
-                     format='json', start_at='earliest') as s:
-        for msg in s:
-            if msg.error() in ['_PARTITION_EOF']:
-                break;
-            messages.append(msg)
+    db.init_app(app)
 
-    return f'{len(messages)} messages found for topic: {id}: {messages}'
+    with app.app_context():
+        db.create_all()
 
 
-@app.route('/message/create', methods=['GET', 'POST'])
-def publish():
-    """
-    Allows a user to publish an alert to the given topic
-    """
-    form = PublishForm(request.form)
-    form.topic.choices = [(topic, topic) for topic in client_wrapper.topics()]
-    if request.method == 'POST' and form.validate():
-        with stream.open(f'kafka://{KAFKA_HOST}:{KAFKA_PORT}/{form.topic.data}', 'w', format='json') as s:
-            s.write({'content': form.content.data})
-    return render_template('publish_form.html', form=form)
+def register_blueprints(app):
+    from routes import routes_bp
+
+    app.register_blueprint(routes_bp)
 
 
-@app.route('/message/<id>', methods=['GET'])
-def message(msg_id):
-    """
-    Displays the content of a specific message
-    """
-    topic = request.args.get('topic')
-
-    return f'message ({msg}) from topic {topic} <not-implemented>'
+app = create_app()
---
+++ routes.py
@@ -0,0 +1,84 @@
+from flask import Blueprint, render_template, request
+
+from scimma.client import stream
+
+from client import ScimmaClientWrapper
+from forms import PublishForm
+
+
+KAFKA_HOST = 'localhost'
+# KAFKA_HOST = 'firkraag.lco.gtn'
+KAFKA_PORT = '9092'
+
+
+kafka_config = {
+    'bootstrap.servers': f'{KAFKA_HOST}:{KAFKA_PORT}',
+    'group.id': 'scimma-web-test',
+    'auto.offset.reset': 'earliest'
+}
+
+client_wrapper = ScimmaClientWrapper(**kafka_config)
+
+
+routes_bp = Blueprint('', 'routes')
+
+
+@routes_bp.route('/', methods=['GET'])
+def index():
+    messages = []
+    topics = client_wrapper.topics()
+    # for topic, topic_metadata in client_wrapper.topics():
+    #     for partition, _ in metadata.partitions.items():
+    #         client_wrapper.consumer.
+    #     print(client_wrapper.current_messages(topic))
+    return 'index'
+
+
+@routes_bp.route('/topic', methods=['GET'])
+def topic_list():
+    """
+    Returns the list of topics and number of messages per topic
+    """
+    topics = [topic for topics, metadata in client_wrapper.topics()]
+    return f'topics: {topics}'
+
+
+@routes_bp.route('/topic/<id>', methods=['GET'])
+def topic_get(id):
+    """
+    Returns the messages for a specific topic
+    """
+    messages = []
+
+    # 'kafka://firkraag.lco.gtn:9092/test',
+    with stream.open(f'kafka://{KAFKA_HOST}:{KAFKA_PORT}/{id}', 'r',
+                     format='json', start_at='earliest') as s:
+        for msg in s:
+            if msg.error() in ['_PARTITION_EOF']:
+                break;
+            messages.append(msg)
+
+    return f'{len(messages)} messages found for topic: {id}: {messages}'
+
+
+@routes_bp.route('/message/create', methods=['GET', 'POST'])
+def publish():
+    """
+    Allows a user to publish an alert to the given topic
+    """
+    form = PublishForm(request.form)
+    form.topic.choices = [(topic, topic) for topic in client_wrapper.topics()]
+    if request.method == 'POST' and form.validate():
+        with stream.open(f'kafka://{KAFKA_HOST}:{KAFKA_PORT}/{form.topic.data}', 'w', format='json') as s:
+            s.write({'content': form.content.data})
+    return render_template('publish_form.html', form=form)
+
+
+@routes_bp.route('/message/<id>', methods=['GET'])
+def message(msg_id):
+    """
+    Displays the content of a specific message
+    """
+    topic = request.args.get('topic')
+
+    return f'message ({msg}) from topic {topic} <not-implemented>'
